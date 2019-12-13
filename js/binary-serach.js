038c7f924e5258c6dcc10b109cfc0afe96bc9af0
NikitinaOlga <https://github.com/Stulnikitina/pracprac>
2019-12-13 08:59:49 +0300

    add return in while

--- js/binary-serach.js
+++ js/binary-serach.js
@@ -2,18 +2,18 @@ function binarySearch(value, list) {
 
     let first = 0;                
     let last = list.length - 1;  
-    let position = -1;
-    let found = false;
+    let index = -1;
     let middle;
 
 
-    while (found === false && first <= last) {
+    while (first <= last) {
 
         middle = Math.floor((first + last) / 2);
 
         if (list[middle] == value) {
             found = true;
-            position = middle;
+            index = middle;
+            return index;
         } else if (list[middle] > value){ 
             last = middle - 1;
         } else {  
@@ -22,10 +22,8 @@ function binarySearch(value, list) {
 
     }
 
-    return position;
-
 }
 
 let list = [1, 3, 6, 8, 16, 25, 45];
-let index = binarySearch(3, list);
+let index = binarySearch(16, list);
 console.log(index);
