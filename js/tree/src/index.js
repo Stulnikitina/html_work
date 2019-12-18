434f562478a917521df93fb8116a8eaba7c8a2c6
NikitinaOlga <https://github.com/Stulnikitina/pracprac>
2019-12-18 22:02:44 +0300

    index.js

--- js/tree/src/index.js
+++ js/tree/src/index.js
@@ -1,17 +1,4 @@
 "use strict";
 
-function tree(height) {
-
-    //обрабатываем тривиальные случаи
-    if (height <= 2 ) { return null; }
-    let tree = "";
-    for(let index = 0; index < height; index++){
-        tree = tree + ' '.repeat(Math.floor(height - index - 1))+ '*'.repeat(2 * index + 1) + ' '.repeat(Math.floor(height - index - 1)) + '\n';
-    }
-
-    tree = tree + ' '.repeat(height - 1) + '|' + ' '.repeat(height - 1) + '\n';
-    return tree;
-
-}
 
 
