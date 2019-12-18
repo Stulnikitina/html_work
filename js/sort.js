f31ebd105fdf2611a024cda2aec93232257c8f04
NikitinaOlga <https://github.com/Stulnikitina/pracprac>
2019-12-18 16:34:43 +0300

    ad sort tree

--- js/sort.js
+++ js/sort.js
@@ -1,11 +1,42 @@
+"use strict";
+
 /**
- * Создаем экземпляр binarySearch.
+ * Создаем экземпляр sort.
+ * Функция sort, которая будет сортировать буквы в словах по алфавиту,
+ * а потом получившиеся слова в предложении — тоже.
+ * Первую букву каждого слова она сделает прописной, остальные — строчными.
  *
  * @author: Nikitina Olga
- * @param {value} - значение, которое мы ищем в отсортированном массиве list 
- *
+ * @param: {sentence} - сортируемое предложение
+ * @return: отсортированный {arrWords}
  */
-function binarySearch() {
 
+let sort = sentence => {
+    let arrWords = sentence.split(" ");
 
+    arrWords = arrWords.map(function(word) {        
+        word.toLowerCase();
+        word = word.split("");
+        word.sort();
+        word = word.join("");
+        if(word.includes("жё")){
+            while (true) {
+                let foundPos = word.indexOf("жё", 0);
+                word = word.replace("жё","ёж");
+                if (foundPos == -1) break;
+
+            }
+        }
+        return `${word[0].toUpperCase()}${word.slice(1)}`;
+    });
+
+    arrWords.sort();
+    arrWords = arrWords.join(" ");
+
+    return arrWords;
 }
+
+
+
+
+
---
+++ js/tree.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function tree(height) {
+
+    if (height <= 2 ) { return null; }
+    let tree = "";
+    for(let index = 0; index < height; index++){
+        tree = tree + ' '.repeat(Math.floor(height - index - 1))+ '*'.repeat(2 * index + 1) + ' '.repeat(Math.floor(height - index - 1)) + '\n';
+    }
+
+    tree = tree + ' '.repeat(height - 1) + '|' + ' '.repeat(height - 1) + '\n';
+    return tree;
+
+}
+
+console.log(tree(5));
+
+
