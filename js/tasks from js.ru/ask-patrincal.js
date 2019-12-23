9541579d993f404fe42c81ca55c7a2c95141ae46
NikitinaOlga <https://github.com/Stulnikitina/pracprac>
2019-12-23 17:43:10 +0300

    tasks

---
+++ js/tasks from js.ru/ask-patrincal.js
@@ -0,0 +1,29 @@
+//https://learn.javascript.ru/task/ask-partial
+"use strict";
+
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+}
+
+let user = {
+    name: 'John',
+
+    login(result) {
+        alert( this.name + (result ? ' logged in' : ' failed to log in') );
+    }
+};
+
+askPassword(user.login.bind(user, true), user.login.bind(user, false)); //тоже самое, что и  пятницу было
+
+
+
+
+
+
+
+
+
+
+
