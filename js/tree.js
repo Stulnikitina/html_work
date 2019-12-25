dcf84c102dd743ed26ed60df4f695d0ff89a6339
NikitinaOlga <https://github.com/Stulnikitina/pracprac>
2019-12-25 12:32:50 +0300

    fix tree.js

--- js/tree.js
+++ js/tree.js
@@ -1,18 +1,20 @@
 "use strict";
 
-function tree(height) {
+/**
+ * @author: Nikitina Olga
+ * @description Функция tree, рисует ASCII-ёлочку высотой N символов из звёздочек.
+ *
+ * @param: {number} height Высота ёлочки
+ * @return: {}
+ */
 
-    if (height <= 2 ) { return null; }
-    let tree = "";
-    for(let index = 0; index < height; index++){
-        tree = tree + ' '.repeat(Math.floor(height - index - 1))+ '*'.repeat(2 * index + 1) + ' '.repeat(Math.floor(height - index - 1)) + '\n';
-    }
-
-    tree = tree + ' '.repeat(height - 1) + '|' + ' '.repeat(height - 1) + '\n';
-    return tree;
-
+let tree = height => {
+    if (height <= 2 ) {return null;}
+    for(let index = 0; index < height; index++) {
+        console.log(' '.repeat(Math.floor(height - index - 1)) + '*'.repeat(2 * index + 1) + ' '.repeat(Math.floor(height - index - 1)));
+    };
+    console.log(' '.repeat(height - 1) + '|' + ' '.repeat(height - 1));
 }
 
-console.log(tree(5));
 
 
