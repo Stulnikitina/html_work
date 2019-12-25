9268c44593c9b3225e3a1c5d7a7d2295eb567f47
NikitinaOlga <https://github.com/Stulnikitina/pracprac>
2019-12-25 16:07:29 +0300

    fix tree

--- js/tree.js
+++ js/tree.js
@@ -8,13 +8,16 @@
  * @return: {}
  */
 
-let tree = height => {
-    if (height <= 2 ) {return null;}
-    for(let index = 0; index < height; index++) {
-        console.log(' '.repeat(Math.floor(height - index - 1)) + '*'.repeat(2 * index + 1) + ' '.repeat(Math.floor(height - index - 1)));
-    };
-    console.log(' '.repeat(height - 1) + '|' + ' '.repeat(height - 1));
-}
+const tree = height => {
 
+    if (height <= 2 ) {return null;}
+
+    let tree = '';
+    for(let index = 0; index < height; index++) {
+        tree = tree + ' '.repeat(Math.floor(height - index - 1)) + '*'.repeat(2 * index + 1) + ' '.repeat(Math.floor(height - index - 1)) + '\n';
+    };
+
+    return tree = tree + ' '.repeat(height - 1) + '|' + ' '.repeat(height - 1) + '\n';
+}
 
 
