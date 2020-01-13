23f54121c8729fb49c7472e7615c9ca3bfd86d30
NikitinaOlga <https://github.com/Stulnikitina/pracprac>
2020-01-13 14:02:11 +0300

    delete easy level

--- try_snake/js/game.js
+++ try_snake/js/game.js
@@ -108,11 +108,6 @@ function drawGame() {
 
   snake.unshift(newHead);
 
-  if (score === 1) {
-    clearInterval(game);
-    let isAgain = confirm("УРА МАША ТЫ ВЫИГРАЛА. Еще раз?");
-    isAgain ? location.reload() : alert(`Жаль. Твой счет: ${score}`);
-  }
 }
 
 let game = setInterval(drawGame, 90);
